c85ca71ba56701710a7686a8e0ab36edf606be4b
Javi Garate <fjgarate@cimne.upc.edu>
2023-01-24 14:33:59 +0100

    CheckDimension

--- structuralstages.gid/structuralstages.tcl
+++ structuralstages.gid/structuralstages.tcl
@@ -5,6 +5,27 @@ namespace eval ::Structural {
 proc GiD_Event_InitProblemtype { dir } {
     Structural::Event_InitProblemtype $dir
 }
+
+proc Structural::Event_InitProblemtype { dir } {
+    GidUtils::OpenWindow CUSTOMLIB
+}
+
 proc Structural::ViewDoc {} {
     W [[customlib::GetBaseRoot] asXML]
+}
+
+proc Structural::ProcgetStateFromXPathValue { domNode args } {
+    set args {*}$args
+    set arglist [split $args " "]
+    set xpath {*}[lindex $arglist 0]
+    set checkvalue [split [lindex $arglist 1] ","]
+    set pst [$domNode selectNodes $xpath]
+    #W "xpath $xpath checkvalue $checkvalue pst $pst"
+    if {$pst in $checkvalue} { return "normal"} else {return "hidden"}
+}
+proc Structural::CheckDimension { domNode args } {
+
+    set checkdim [lindex $args 0]
+
+    if {$checkdim eq "3D"} {return "normal"} else {return "hidden"}
 }
