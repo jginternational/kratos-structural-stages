d49dcee9c4134fcff193afc55904e557a2d0d609
Javi Garate <fjgarate@cimne.upc.edu>
2023-01-24 14:01:26 +0100

    tree development

--- structuralstages.gid/structuralstages.tcl
+++ structuralstages.gid/structuralstages.tcl
@@ -1,7 +1,3 @@
-##################################################################################
-#   This file is common for all Kratos Applications.
-#   Do not change anything here unless it's strictly necessary.
-##################################################################################
 
 namespace eval ::Structural {
 }
@@ -9,20 +5,6 @@ namespace eval ::Structural {
 proc GiD_Event_InitProblemtype { dir } {
     Structural::Event_InitProblemtype $dir
 }
-
-proc Structural::Events { } {
-    Structural::RegisterGiDEvents
-}
-
-proc Structural::RegisterGiDEvents { } {
-}
-
-proc Structural::Event_InitProblemtype { dir } {
-    variable kratos_private
-
-
-}
-
-proc Structural::LoadCommonScripts { } {
-
+proc Structural::ViewDoc {} {
+    W [[customlib::GetBaseRoot] asXML]
 }
